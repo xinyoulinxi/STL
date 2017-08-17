3d568df667729d86b3a98be4f774584c3c2659a6
YL <1135479396@qq.com>
2017-08-17 20:42:19 +0800

    添加fill_n()函数

--- mySTL/Algorithm.h
+++ mySTL/Algorithm.h
@@ -3,7 +3,8 @@
 #include<cstring>
 namespace STL {
 	/**********************              fill()     ***********************************
-	**************************** Algorithm Complexity  O(N)  ******************************/                  /
+	**************************** Algorithm Complexity  O(N)  ******************************/ 
+
 	template<class ForwardIterator, class T>
 	void fill(ForwardIterator first, ForwardIterator last, const T& value)
 	{
@@ -18,6 +19,30 @@ namespace STL {
 	{
 		memset(first, static_cast<unsigned char>(value), (last - first) * sizeof(wchar_t));
 	}
+
+	/**********************              fill_n()     ***********************************
+	**************************** Algorithm Complexity  O(N)  ******************************/
+
+	template<class Iterator, class Size, class T>
+	Iterator fill_n(Iterator first, Size n, const T& value)
+	{
+		for (; n > 0; --n, ++first)
+			*first = value;
+		return first;
+	}
+	template<class Size>
+	char *fill_n(char *first, Size n, const char& value)
+	{
+		memset(first, static_cast<unsigned char>(value), n);
+		return first + n;
+	}
+	template<class Size>
+	wchar_t *fill_n(wchar_t *first, Size n, const wchar_t& value)
+	{
+		memset(first, static_cast<unsigned char>(value), n * sizeof(wchar_t));
+		return first + n;
+	}
+
 }
 
 #endif
