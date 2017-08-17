c6eed8b34ebeb6f508d971568d76467c765c3c73
YL <1135479396@qq.com>
2017-08-17 21:03:53 +0800

    添加注释，并定义了pop_back()和clear()函数

--- mySTL/Vector_detail.h
+++ mySTL/Vector_detail.h
@@ -5,20 +5,24 @@ namespace STL {
 	//*********************************构造、析构**************************************
 
 	template<class T, class Alloc>
-	vector<T, Alloc>::vector(const size_type n) {
-		
+	vector<T, Alloc>::~vector() {
+		deallocate();
 	}
-
 
 	template<class T, class Alloc>
-	vector<T, Alloc>::vector(size_type n, const value_type& value) {
-
+	vector<T, Alloc>::vector(const size_type n) {
+		allocate_and_fill_n(n, value_type());
 	}
-	//空间配置器相关
+	template<class T, class Alloc>
+	vector<T, Alloc>::vector(size_type n, const value_type& value) {
+		allocate_and_fill_n(n, value);
+	}
+
+	//********************************空间配置器相关*********************
 	template<class T, class Alloc>
 	void vector<T, Alloc>::fill_initialize(size_t n, const value_type& value) {
 		allocate_and_fill_n(n, value);
-		finish_ =start_ + n;
+		finish_ = start_ + n;
 		end_of_storage = finish_;
 	}
 
@@ -30,7 +34,7 @@ namespace STL {
 	}
 	template<class T, class Alloc>
 	void vector<T, Alloc>::deallocate() {
-		if (start_) {
+		if (start_) {//存在被配置过的空间
 			data_Allocator::destory(start_,finish_);
 			data_Allocator::deallocate(start_, capacity());
 		}
@@ -40,6 +44,20 @@ namespace STL {
 	void vector<T, Alloc>::reallocate(size_t n = 0) {
 
 	}
-	
+	template<class T, class Alloc>
+	void vector<T, Alloc>::clear() {
+		data_Allocator::destory(start_, finish_);
+		finish_ = start_;
+	}
+	template<class T, class Alloc>
+	void vector<T, Alloc>::pop_back() {
+		--finish_;
+		data_Allocator::destroy(finish_);
+	}
+
+	//*******************************对容器进行修改的内部函数******************
+
+
+	//*******************************容器容量相关的操作函数********************
 }
 #endif
