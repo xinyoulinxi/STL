0266f4f1370239c9fc214657095cd12e253ac2ed
YL <1135479396@qq.com>
2017-09-10 18:15:59 +0800

    实现了deque的几个元素访问函数和init函数

--- mySTL/Deque_detail.h
+++ mySTL/Deque_detail.h
@@ -5,7 +5,7 @@ namespace STL {
 	namespace Detail {
 
 		template<class T>
-		deque_iterator<T> & deque_iterator<T>::operator = (const deque_iterator & it){
+		deque_iterator<T> & deque_iterator<T>::operator = (const deque_iterator & it) {
 			if (this != &it) {
 				this->cur_ = it.cur_;
 				this->mapIndex_ = it.mapIndex_;
@@ -13,43 +13,43 @@ namespace STL {
 			}
 			return *this;
 		}
-		
+
 		template<class T>
 		deque_iterator<T>  & deque_iterator<T>::operator++() {
-			if (cur_ != getNowBuckTail(mapIndex_)) {//+1之后依然在桶内
+			if (cur_ != getNowBuckTail()) {//+1之后依然在桶内
 				++cur;
 			}
-			else if (mapIndex_ + 1 < container_->map_size_) {//已经在桶的结尾，但是之后还有新的map指针
+			else if (mapIndex_ + 1 < container_->mapSize_) {//已经在桶的结尾，但是之后还有新的map指针
 				++mapIndex_;
-				cur_ = getNowBuckHead(mapIndex_);//指向下一个桶的开头
+				cur_ = getNowBuckHead();//指向下一个桶的开头
 			}
 			else {//mapIndex_ +1之后没有了map
-				mapIndex_ = container_->map_size_;
+				mapIndex_ = container_->mapSize_;
 
 				cur_ = container_->map_[mapIndex_];//指向未知区域
 			}
 			return *this;
 		}
 		template<class T>
-		deque_iterator<T> deque_iterator<T>::operator++(int){
+		deque_iterator<T> deque_iterator<T>::operator++(int) {
 			auto tmp = *this;
 			++*this;
 			return tmp;
 		}
 		template<class T>
-		deque_iterator<T>  & deque_iterator<T>::operator--(){
-			if (cur_ != getNowBuckHead(mapIndex_)) {
+		deque_iterator<T>  & deque_iterator<T>::operator--() {
+			if (cur_ != getNowBuckHead()) {
 				--cur_;
 			}
 			else if (mapIndex_ - 1 >= 0) {
 				--mapIndex_;
-				cur_ = getNowBuckHead(mapIndex_);
+				cur_ = getNowBuckHead();
 			}
 			else {
 				mapIndex = 0;
 				cur_ = container_->map_[mapIndex_];
 			}
-			
+
 		}
 		template<class T>
 		deque_iterator<T> deque_iterator<T>::operator--(int)
@@ -59,45 +59,116 @@ namespace STL {
 			return retIter;
 		}
 		template<class T>
-		bool deque_iterator<T>::operator==(const deque_iterator & rhs) const{
+		bool deque_iterator<T>::operator==(const deque_iterator & rhs) const {
 			return ((mapIndex_ == rhs.mapIndex_)
 				&& (cur_ == rhs.cur_)
 				&& (container_ == rhs.container_));
 		}
 		template<class T>
-		inline bool deque_iterator<T>::operator!=(const deque_iterator & rhs) const{
+		inline bool deque_iterator<T>::operator!=(const deque_iterator & rhs) const {
 			return !(rhs == *this);
 		}
 		template<class T>
-		deque_iterator<T> deque_iterator<T>::getNowBuckTail(size_t mapIndex)
+		deque_iterator<T> deque_iterator<T>::getNowBuckTail()
 		{
-			return container_->map_[mapIndex] + (container_->getBuckSize() - 1);
+			return container_->map_[mapIndex_] + (container_->getBuckSize() - 1);
 		}
 		template<class T>
-		deque_iterator<T> deque_iterator<T>::getNowBuckHead(size_t mapIndex)
+		deque_iterator<T> deque_iterator<T>::getNowBuckHead()
 		{
-			return container_->map_[mapIndex]);
+			return container_->map_[mapIndex_];
 		}
 		template<class T>
-		inline size_t deque_iterator<T>::getBuckSize(){
+		inline size_t deque_iterator<T>::getBuckSize() {
 			return container_->getBuckSize();
 		}
 
 	}//end of Detail
 
 	template<class T, class Alloc>
-	T * deque<T, Alloc>::getANewBuck()
-	{
-
+	T * deque<T, Alloc>::getANewBuck(){
+		return dataAllocator::allocate(getBuckSize());//未构造空间
 	}
 	template<class T, class Alloc>
 	T ** deque<T, Alloc>::getANewMap(const size_t size){
+		T **map = new T*[size];
+		for (int i = 0; i != size; ++i) {
+			map[i] = getANewBuck();
+		}
+		return map;
+	}
+	template<class T, class Alloc>
+	size_t deque<T, Alloc>::getNewMapSize(const size_t size){
+		return (size == 0 ? 2 : size + 2);
+	}
+	//元素访问
+	template<class T, class Alloc>
+	deque<T, Alloc>::reference deque<T, Alloc>::operator[] (size_type n) {
+		return *(begin() + n);
+	}
+	template<class T, class Alloc>
+	deque<T, Alloc>::reference deque<T, Alloc>::front() {
+		return *begin();
+	}
+	template<class T, class Alloc>
+	deque<T, Alloc>::reference deque<T, Alloc>::back() {
+		return *(end() - 1);
+	}
+	//构造函数相关
+	template<class T, class Alloc>
+	deque<T, Alloc>::deque() {
+		
+		
+	}
+		
+	template<class T, class Alloc>
+	deque<T, Alloc>::deque(size_t n, const value_type & value = value_type()) {
 
 	}
 	template<class T, class Alloc>
-	size_t deque<T, Alloc>::getNewMapSize(const size_t size)
-	{
+	
+	template <class InputIterator>
+	deque<T, Alloc>::deque(InputIterator first, InputIterator last) {
 
+	}
+	template<class T, class Alloc>
+	deque<T, Alloc>::deque(const deque& rhs) {
+
+	}
+	template<class T, class Alloc>
+	deque<T, Alloc>::~deque() {
+
+	}
+
+	//元素操作相关
+	template<class T, class Alloc>
+	void deque<T, Alloc>::push_back() {
+
+	}
+	template<class T, class Alloc>
+	void deque<T, Alloc>::deque<T, Alloc>::pop_back() {
+
+	}
+	template<class T, class Alloc>
+	void deque<T, Alloc>::push_front() {
+
+	}
+	template<class T, class Alloc>
+	void deque<T, Alloc>::pop_front() {
+
+	}
+	template<class T, class Alloc>
+	void deque<T, Alloc>::clear() {
+
+	}
+	template<class T, class Alloc>
+	void deque<T, Alloc>::init() {
+		mapSize_ = 2;
+		map_ = getANewMap();
+		//将起始点放置在中间
+		start_.container_ = finish_.container_ = this;
+		start_.mapIndex_ = finish_.mapIndex_ = mapSize_ - 1;
+		start_.cur_ = finish_.cur_ = map_[mapSize_ - 1];
 	}
 }
 
