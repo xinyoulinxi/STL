8612eb69e1bc59a24622c7c478b949f752f42939
YL <1135479396@qq.com>
2017-09-17 18:11:41 +0800

    添加了queue类，并实现了部分函数操作以及测试

---
+++ mySTL/Queue.h
@@ -0,0 +1,72 @@
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+#include"Deque.h"
+namespace STL {
+	template<class T,class Container=STL::deque<T>>
+	class queue {
+	public:
+		typedef T                   value_type;
+		typedef value_type*          pointer;
+		typedef pointer              iterator;
+		typedef value_type&           reference;
+		typedef const reference      const_reference;
+		typedef  size_t           size_type;
+		typedef Container       container_type;
+	private:
+		container_type container_;
+	public:
+		queue(container_type conr = container_type()) :container_(conr) {}
+		
+		reference front();
+		reference back();
+		const_reference front()const;
+		const_reference back()const;
+		void push(const value_type& value);
+		void pop();
+		bool empty()const;
+		size_type size()const;
+
+	};
+	template<class T, class Container>
+	inline queue<T, Container>::reference queue<T, Container>::front()
+	{
+		return container_.front();
+	}
+	template<class T, class Container>
+	inline queue<T, Container>::reference queue<T, Container>::back()
+	{
+		return container_.back();
+	}
+	template<class T, class Container>
+	inline queue<T, Container>::const_reference queue<T, Container>::front() const
+	{
+		return container_.front();
+	}
+	template<class T, class Container>
+	inline queue<T, Container>::const_reference queue<T, Container>::back() const
+	{
+		return container_.back();
+	}
+	template<class T, class Container>
+	inline void queue<T, Container>::push(const value_type& value)
+	{
+		container_.push_back(value);
+	}
+	template<class T, class Container>
+	inline void queue<T, Container>::pop()
+	{
+		container_.pop_front();
+	}
+	template<class T, class Container>
+	inline bool queue<T, Container>::empty() const
+	{
+		return container_.empty();
+	}
+	template<class T, class Container>
+	inline queue<T, Container>::size_type queue<T, Container>::size() const
+	{
+		return container_.size();
+	}
+}
+#endif // !_QUEUE_H_
+
